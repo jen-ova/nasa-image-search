6fb3436a5d21c6b907038fc87be48b969956dbe2
Jen Stewart <jenstewart2404@gmail.com>
2021-10-12 14:58:27 +0100

    added getImages request and imported axios for get requests to nasa api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
+import getImages from "../requests/getImages";
 import "../styles/Search.css";
 
 function Search() {
   const [value, setValue] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
     <>
-      <form>
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-input"
           type="text"
           onChange={(event) => setValue(event.target.value)}
-          value={value}
         />
         <button type="submit" className="search-button">
           Search
